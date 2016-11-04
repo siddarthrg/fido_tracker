c0de0c6442b8cd264efa0be454fba9e86851fb32
Siddarth RG <siddarthrg@gmail.com>
2016-11-04 12:50:09 +0530

    renamed food.json

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 /**
- * Introduction to Human-Computer Interaction
- * Lab 2
+ * Fido App
  * --------------
- * Created by: Michael Bernstein
- * Last updated: December 2013
+ * ELim, KKameda, SGovindan
+ * Based off Lab code from COGS 120 @ UCSD
  */
 var PORT = 3000;
 
 // Express is a web framework for node.js
 // that makes nontrivial applications easier to build
 var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars')
+
+//var index = require('./routes/index');
+//var project = require('./routes/project');
+// Example route
+// var user = require('./routes/user');
 
 // Create the server instance
 var app = express();
@@ -17,6 +24,36 @@ var app = express();
 // Print logs to the console and compress pages we send
 app.use(express.logger());
 app.use(express.compress());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// Add routes here
+
+// Example route
+// app.get('/users', user.list);
+
+/*
+ * http.createServer(app).listen(app.get('port'), function(){
+ * console.log('Express server listening on port ' + app.get('port'));
+ * });
+ */
 
 // Return all pages in the /static directory
 // whenever they are requested at '/'
@@ -29,3 +66,4 @@ var port = process.env.PORT || PORT; // 80 for web, 3000 for development
 app.listen(port, function() {
 	console.log("Node.js server running on port %s", port);
 });
+
