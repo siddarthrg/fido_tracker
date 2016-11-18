d7d160f19a51446a6f5d61ea2c1eeae88b84c23e
Siddarth RG <siddarthrg@gmail.com>
2016-11-18 07:21:40 +0530

    still not working gtg for midterm

--- routes/feed_save.js
+++ routes/feed_save.js
@@ -7,23 +7,23 @@ exports.view = function(req, res){
 
 	// Set ateFoodGroup to numerical values
 	var ateGrains;
-	if(req.body.ateGrains == ""){ateGrains = 0;}
+	if(req.body.ateGrains == null){ateGrains = 0;}
 	else ateGrains = 1; 
 
 	var ateFruit;
-	if(req.body.ateFruit == ""){ateFruit = 0;}
+	if(req.body.ateFruit == null){ateFruit = 0;}
 	else ateFruit = 1; 
 
 	var ateDairy;
-	if(req.body.ateDairy == ""){ateDairy = 0;}
+	if(req.body.ateDairy == null){ateDairy = 0;}
 	else ateDairy = 1; 
 
 	var ateProtein;
-	if(req.body.ateProtein == ""){ateProtein = 0;}
+	if(req.body.ateProtein == null){ateProtein = 0;}
 	else ateProtein = 1; 
 
 	var ateVeggies;
-	if(req.body.ateVeggies == ""){ateVeggies = 0;}
+	if(req.body.ateVeggies == null){ateVeggies = 0;}
 	else ateVeggies = 1; 
 	
 	// Creat new JSON and append to log
